0b8473cd686ca3e62b929a90d00d4a9cad330847
Jzf <sreenath.joseph@gmail.com>
2019-08-29 07:06:11 +0530

    Basic string manipulation, sub-strings

--- quotes.py
+++ quotes.py
@@ -9,3 +9,15 @@ Here is our first email to you.
 Thank you,
 Joe
 ''')
+
+course = 'Python for Beginners'
+
+print(course)
+print(course[0])
+print(course[0:3])
+print(course[2:3])
+print(course[2:])
+print(course[:3])
+print(course[:-1])
+print(course[-4:-1])
+print(course[-4:])
